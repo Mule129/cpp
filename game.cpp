4f66fcd0dd778a96fc310fa41c2e9a811ef76d57
Mule129 <mule01129@gmail.com>
2024-04-07 21:28:30 +0900

    [Init] initialize tetris game

---
+++ game.cpp
@@ -0,0 +1,21 @@
+#include "game.h"
+
+Game::Game() {
+
+}
+
+void Game::update() {
+    int a = 0;
+}
+
+// 게임 화면을 그린다.
+void Game::draw() {
+    int a = 0;
+}
+
+// 게임 루프가 종료되어야 하는지 여부를 반환한다.
+bool Game::shouldExit() {
+    return false;
+}
+
+  
